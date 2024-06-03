dc4eb4cb651a6502265e6d79300ee97745fb06a2
EmiRoberti77 <emiliano.roberti@accenture.com>
2024-06-03 06:56:18 +0100

    added new embedding models:

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ import { embedding_model } from './share';
 dotenv.config();
 
 const embedder = new Embedder({
-  input: 'Phil Emi and Vincenzo',
-  model: embedding_model.text_embedding_ada_002,
+  input: 'emi is testing embedding',
+  model: embedding_model.text_embedding_3_small,
   key: process.env.OPENAI_API_KEY || 'noKey',
 });
 
--- src/share.ts
+++ src/share.ts
@@ -1,3 +1,5 @@
 export enum embedding_model {
   text_embedding_ada_002 = 'text-embedding-ada-002',
+  text_embedding_3_small = 'text-embedding-3-small',
+  text_embedding_3_large = 'text-embedding-3-large',
 }
